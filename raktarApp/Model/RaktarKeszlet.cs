bde231fd3fa32428b4e7035227720d53a0e38e88
sze1bakpet@vasvari.org <sze1bakpet@vasvari.org>
2022-04-15 12:28:48 +0200

    RaktarKeszlet const,getter,setter

--- raktarApp/Model/RaktarKeszlet.cs
+++ raktarApp/Model/RaktarKeszlet.cs
@@ -8,6 +8,49 @@ namespace raktarApp.Model
 {
     public class RaktarKeszlet
     {
+        private string termeknev;
+        private int mennyiseg;
+        private string mennyisegmertek;
+        private int egysegar;
 
+        public RaktarKeszlet(string termeknev, int mennyiseg, string mennyisegmertek,int egysegar)
+        {
+            this.termeknev = termeknev;
+            this.mennyiseg = mennyiseg;
+            this.mennyisegmertek = mennyisegmertek;
+            this.egysegar = egysegar;
+        }
+
+        public string Termeknev
+        {
+            get
+            { return Termeknev; }
+            set
+            { Termeknev = value; }
+        }
+
+        public int Mennyiseg
+        {
+            get
+            { return Mennyiseg; }
+            set
+            { Mennyiseg = value; }
+        }
+
+        public string Mennyisegmertek
+        {
+            get 
+            { return Mennyisegmertek; }
+            set
+            { Mennyisegmertek = value; }
+        }
+
+        public int Egysegar
+        {
+            get
+            { return egysegar; }
+            set
+            { egysegar = value; }
+        }
     }
 }
